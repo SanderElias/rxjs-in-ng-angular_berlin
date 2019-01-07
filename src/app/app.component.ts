dc91aa4861fd4b67450bf2c4fade79259fae62a3
Sander Elias <sanderelias+git@gmail.com>
2019-01-07 10:39:34 +0100

    Updated deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
-import {} from '';
 import { RakiService } from './rijks/raki.service';
 import { SwUrlService } from './samples/sw-url.service';
 import { timer, fromEvent, Subject, empty, of } from 'rxjs';
